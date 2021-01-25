efb0564e311d00998fc1132d0cfce99e9461f2f2
sagar jiwtani <sagar_jiwtani@persistent.com>
2021-01-25 15:35:58 -0800

    added math.py

---
+++ math.py
@@ -0,0 +1,21 @@
+# Perform addition
+def add(x,y):
+    pass
+# Perform subtraction
+def subtract(x,y):
+    pass
+# Perform multiplication
+def multiply(x,y):
+    return x*y
+    
+# Perform division
+def divide(x,y):
+	if y==0:
+		return error
+<<<<<<< HEAD
+    return x/y
+=======
+    return x/y
+    def square(n)
+    return n*n
+>>>>>>> a6fbde8 (square imple)
